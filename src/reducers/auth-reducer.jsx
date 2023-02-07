9eed2483097692ee532bef0521b1de64078875d7
Raksha Pawar <rakshapawar10.sae@gmail.com>
2023-02-07 08:22:16 +0530

    feat: Add authReducer with three actions for login

---
+++ src/reducers/auth-reducer.jsx
@@ -0,0 +1,26 @@
+const authReducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_INITIAL_STATE":
+      return {
+        ...state,
+        user: action.payload.user,
+        token: action.payload.token,
+      };
+
+    case "LOGIN_SUCCESS":
+      return {
+        ...state,
+        user: action.payload.user,
+        token: action.payload.token,
+      };
+
+    case "LOGOUT":
+      return {
+        ...state,
+        user: {},
+        token: {},
+      };
+  }
+};
+
+export { authReducer };

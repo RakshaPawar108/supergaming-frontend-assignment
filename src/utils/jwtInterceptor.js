cfb75d0fb6699de644e9af805287988363563140
Raksha Pawar <rakshapawar10.sae@gmail.com>
2023-02-11 11:52:41 +0530

    fix: Remove jwtInterceptor for request

--- src/utils/jwtInterceptor.js
+++ src/utils/jwtInterceptor.js
@@ -2,10 +2,4 @@ import axios from "axios";
 
 const jwtInterceptor = axios.create({});
 
-jwtInterceptor.interceptors.request.use((config) => {
-  let accToken = localStorage.getItem("accessToken");
-  config.headers.common["Authorization"] = `Bearer ${accToken}`;
-  return config;
-});
-
 export { jwtInterceptor };

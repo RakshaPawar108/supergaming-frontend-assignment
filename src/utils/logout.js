ae4ce3995aa5815dd1ef90c5342bd751acf87c5b
Raksha Pawar <rakshapawar10.sae@gmail.com>
2023-02-11 11:32:53 +0530

    fix: async await syntax in logout file

--- src/utils/logout.js
+++ src/utils/logout.js
@@ -2,7 +2,7 @@ import { jwtInterceptor } from "./jwtInterceptor";
 
 export const logout = async () => {
   try {
-    const response = jwtInterceptor.post("https://test.indusgame.com/logouts");
+    const response = await jwtInterceptor.post("https://test.indusgame.com/logouts");
 
     return response;
   } catch (err) {

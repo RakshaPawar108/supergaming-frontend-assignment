c9ca3c1b01df540af1d7b6696984a6b863c6fc10
Raksha Pawar <rakshapawar10.sae@gmail.com>
2023-02-11 19:02:18 +0530

    style: Add styling and structure to Logout page

--- src/pages/LogoutPage/LogoutPage.jsx
+++ src/pages/LogoutPage/LogoutPage.jsx
@@ -1,3 +1,29 @@
+import { Link } from "react-router-dom";
+
 export const LogoutPage = () => {
-  return <div>Logout Page</div>;
+  return (
+    <div class="ui centered card">
+      <div class="content">
+        <i
+          class="check icon check-icon"
+          style={{
+            fontSize: "1.5rem",
+            marginLeft: "47%",
+            marginTop: "2rem",
+            color: "green",
+          }}
+        ></i>
+        <h3 class="ui center aligned header">
+          You have been successfully logged out
+        </h3>
+        <Link
+          to="/login"
+          class="ui center aligned sub header"
+          style={{ fontSize: "1rem", margin: "1rem" }}
+        >
+          Go to Login Page
+        </Link>
+      </div>
+    </div>
+  );
 };

b026ae6b4edb2b6cd4aa482bc90e8117461e8e38
Raksha Pawar <rakshapawar10.sae@gmail.com>
2023-02-12 16:15:49 +0530

    fix: Update code to get object property from localStorage

--- src/utils/logout.js
+++ src/utils/logout.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const logout = async () => {
-  let accToken = localStorage.getItem("accessToken");
+  let auth = JSON.parse(localStorage.getItem("auth"));
+  let accToken = auth.accessToken;
   try {
     const response = await axios.post(
       "https://test.indusgame.com/logouts",

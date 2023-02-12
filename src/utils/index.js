62d3509f8b0ff1bcfea9b740280bd032e076c5a3
Raksha Pawar <rakshapawar10.sae@gmail.com>
2023-02-12 16:13:58 +0530

    fix: Remove unnecessary paths from index exports

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,3 @@
 export { provideAuth } from "./provideAuth";
 export { refreshAccessToken } from "./refreshAccessToken";
-export { jwtInterceptor } from "./jwtInterceptor";
 export { logout } from "./logout";

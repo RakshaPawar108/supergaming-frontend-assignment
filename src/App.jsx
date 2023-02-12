dda61d032da651a9e99e4555b2fbe253109ad1e9
Raksha Pawar <rakshapawar10.sae@gmail.com>
2023-02-12 16:16:44 +0530

    feat: Add refreshToken functionality on page load and if expired token

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,67 @@ import { Header, LoginForm } from "./components";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Home, LogoutPage } from "./pages";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useAuth } from "./context";
+import { refreshAccessToken } from "./utils";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(
+    localStorage.getItem("isLoggedIn") || false
+  );;
+  const { authState, authDispatch } = useAuth();
+
+  const handleRefreshAccessToken = async () => {
+    try {
+      if (
+        !authState.auth?.accessToken ||
+        Date.now() >= authState.auth.expiresInSeconds * 1000
+      ) {
+        const response = await refreshAccessToken(authState.auth.refreshToken);
+        if (response?.status === 200) {
+          let authData = JSON.parse(localStorage.getItem("auth"));
+          authData.accessToken = response.data.accessToken;
+          authData.refreshToken = response.data.refreshToken;
+          authData.expiresInSeconds = response.data.expiresInSeconds;
+
+          localStorage.setItem("auth", JSON.stringify(authData));
+
+          // update auth state in hook
+          authDispatch({
+            type: "TOKEN_REFRESH",
+            payload: {
+              auth: {
+                accessToken: response.data.accessToken,
+                refreshToken: response.data.refreshToken,
+                expiresInSeconds: response.data.expiresInSeconds,
+              },
+            },
+          });
+        }
+      }
+      setTimeout(
+        handleRefreshAccessToken,
+        (authState.auth.expiresInSeconds - 60) * 1000
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    if (authState.auth?.accessToken) {
+      handleRefreshAccessToken();
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("isLoggedIn", isLoggedIn);
+  }, [isLoggedIn]);
+
+
   return (
     <div className="App">
-      <Header />
+      <Header setIsLoggedIn={setIsLoggedIn} isLoggedIn={isLoggedIn} />
       <ToastContainer
         position="top-right"
         autoClose={3000}
--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./LoginForm.css";
-import { provideAuth, refreshAccessToken } from "../../utils";
+import { provideAuth } from "../../utils";
 import { useAuth } from "../../context";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router";
@@ -8,9 +8,6 @@ import { useNavigate } from "react-router";
 export const LoginForm = ({ setIsLoggedIn }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [refreshToken, setRefreshToken] = useState("");
-  const [accessToken, setAccessToken] = useState("");
-  const [expiresInSeconds, setExpiresInSeconds] = useState(0);
   const { authDispatch } = useAuth();
   const navigate = useNavigate();
 
@@ -25,15 +22,7 @@ export const LoginForm = ({ setIsLoggedIn }) => {
 
       if (response.status === 200) {
         localStorage.setItem("user", JSON.stringify(response.data.user));
-        localStorage.setItem("accessToken", response.data.auth.accessToken);
-        localStorage.setItem(
-          "expiresInSeconds",
-          response.data.auth.expiresInSeconds
-        );
-        setAccessToken(response.data.auth.accessToken);
-        setExpiresInSeconds(response.data.auth.expiresInSeconds);
-        setRefreshToken(response.data.auth.refreshToken);
-        console.log(accessToken);
+        localStorage.setItem("auth", JSON.stringify(response.data.auth));
 
         authDispatch({
           type: "LOGIN_SUCCESS",
@@ -55,38 +44,6 @@ export const LoginForm = ({ setIsLoggedIn }) => {
       console.log(err);
     }
   };
-
-  const handleRefreshAccessToken = async () => {
-    try {
-      const response = await refreshAccessToken(refreshToken);
-      if (response.status === 200) {
-        setRefreshToken(response.data.refreshToken);
-        let accToken = localStorage.getItem("accessToken");
-        accToken = response.data.accessToken;
-        localStorage.setItem("accessToken", accToken);
-        let expInSecs = localStorage.getItem("expiresInSeconds");
-        expInSecs = response.data.expiresInSeconds;
-        localStorage.setItem("expiresInSeconds", expInSecs);
-        setAccessToken(response.data.accessToken);
-        setExpiresInSeconds(response.data.expiresInSeconds);
-        setRefreshToken(response.data.refreshToken);
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      if (expiresInSeconds && refreshToken) {
-        handleRefreshAccessToken();
-        console.log(expiresInSeconds);
-      }
-    }, (expiresInSeconds - 60) * 1000); // Refresh access token 60 seconds before it expires
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, []);
 
   return (
     <div className="ui middle aligned centered card login-form">

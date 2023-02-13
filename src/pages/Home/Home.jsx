92b829f3ab39d3cb906986f62e979336e760573e
Raksha Pawar <rakshapawar10.sae@gmail.com>
2023-02-13 22:31:28 +0530

    refactor: Add fetch call on loading the home component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,28 @@
+import { useEffect, useState } from "react";
+import { fetchUnits } from "../../services";
+import { toast } from "react-toastify";
+
 export const Home = () => {
+  const [units, setUnits] = useState([]);
+
+  const authData = JSON.parse(localStorage.getItem("auth"));
+
+  const getUnits = async () => {
+    try {
+      const response = await fetchUnits(authData.accessToken);
+      if (response.status === 200) {
+        setUnits(response.data);
+      }
+    } catch (err) {
+      toast.error("Error in fetching units: " + err.message, {
+        theme: "dark",
+      });
+    }
+  };
+
+  useEffect(() => {
+    getUnits();
+  }, []);
+
   return <div>This is the Home page</div>;
 };

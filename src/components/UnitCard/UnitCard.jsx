343d845ccbcd21899d09406c63a34f83f961da85
Raksha Pawar <rakshapawar10.sae@gmail.com>
2023-02-14 13:10:33 +0530

    style: Add styling to unit cards

--- src/components/UnitCard/UnitCard.jsx
+++ src/components/UnitCard/UnitCard.jsx
@@ -1,28 +1,39 @@
 import { Link } from "react-router-dom";
+import "./UnitCard.css";
 
 export const UnitCard = ({ unit }) => {
   return (
     <div className="card">
       <div className="image">
-        <img src={unit.imageUrl} alt={unit.name} />
+        <img className="unit-image" src={unit.imageUrl} alt={unit.name} />
       </div>
       <div className="content">
-        <div className="header">{unit.name}</div>
-        <div className="meta">
+        <div
+          className="header unit-name"
+          style={{ fontSize: "1.2rem", fontFamily: "Poppins" }}
+        >
+          {unit.name}
+        </div>
+        <div className="sub header">
           <a>Code: {unit.code}</a>
         </div>
-        <div className="description">{unit.description}</div>
-        <div className="labels">
-          <a class="ui orange tiny label">{unit.type}</a>
-          <a class="ui violet tiny label">{unit.role}</a>
-          <a class="ui pink tiny label">{unit.faction}</a>
-          <a class="ui yellow tiny label">{unit.attackType}</a>
-        </div>
-        <div className="description">
+        <div className="description unit-description">{unit.description}</div>
+        <div className="description ability-description">
           <strong>Ability:</strong> {unit.ability.name}
         </div>
       </div>
-      <div className="extra content">
+      <div className="content">
+        <div className="labels">
+          <a className="ui orange tiny label">{unit.type}</a>
+          <a className="ui violet tiny label">{unit.role}</a>
+          <a className="ui pink tiny label">{unit.faction}</a>
+          <a className="ui red tiny label">{unit.attackType}</a>
+        </div>
+      </div>
+      <div
+        className="extra content unit-extra-content"
+        style={{ fontSize: "0.8rem" }}
+      >
         <Link to="/" className="right floated">
           See More
         </Link>

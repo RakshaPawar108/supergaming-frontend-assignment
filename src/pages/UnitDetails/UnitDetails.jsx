261e512e5876ca8ab3150cd33fea45afed73f1b0
Raksha Pawar <rakshapawar10.sae@gmail.com>
2023-02-14 19:03:20 +0530

    feat: Add UnitDetails page with 2 col layout and dummy text

---
+++ src/pages/UnitDetails/UnitDetails.jsx
@@ -0,0 +1,8 @@
+export const UnitDetails = () => {
+  return (
+    <div class="ui grid">
+      <div class="eight wide column">Col 1</div>
+      <div class="eight wide column">Col 2</div>
+    </div>
+  );
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,3 @@
 export { Home } from "./Home/Home";
 export { LogoutPage } from "./LogoutPage/LogoutPage";
+export { UnitDetails } from "./UnitDetails/UnitDetails";

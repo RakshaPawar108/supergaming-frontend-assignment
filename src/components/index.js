25f86da37b01dc75fa616d4c18d05a74d12d32df
Raksha Pawar <rakshapawar10.sae@gmail.com>
2023-02-16 12:51:06 +0530

    feat: Add Loader component

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { Header } from "./Header/Header";
 export { UnitCard } from "./UnitCard/UnitCard";
 export { ModalComponent } from "./ModalComponent/ModalComponent";
 export { CodeSort } from "./CodeSort/CodeSort";
+export { Loader } from "./Loader/Loader";
